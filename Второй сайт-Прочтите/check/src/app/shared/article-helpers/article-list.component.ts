fef60fa6bd08911663eef1a039a541ffa7620515
topprojectteam <m.binazar_98@mail.ru>
2020-04-09 14:00:27 +0600

    1

--- Второй сайт-Прочтите/check/src/app/shared/article-helpers/article-list.component.ts
+++ Второй сайт-Прочтите/check/src/app/shared/article-helpers/article-list.component.ts
@@ -36,7 +36,7 @@ export class ArticleListComponent {
     this.loading = true;
     this.results = [];
 
-    // Create limit and offset filter (if necessary)
+
     if (this.limit) {
       this.query.filters.limit = this.limit;
       this.query.filters.offset =  (this.limit * (this.currentPage - 1));
@@ -47,7 +47,7 @@ export class ArticleListComponent {
       this.loading = false;
       this.results = data.articles;
 
-      // Used from http://www.jstips.co/en/create-range-0...n-easily-using-one-line/
+
       this.totalPages = Array.from(new Array(Math.ceil(data.articlesCount / this.limit)), (val, index) => index + 1);
     });
   }
--- Второй сайт-Прочтите/check/src/app/shared/buttons/follow-button.component.ts
+++ Второй сайт-Прочтите/check/src/app/shared/buttons/follow-button.component.ts
@@ -22,7 +22,7 @@ export class FollowButtonComponent {
 
   toggleFollowing() {
     this.isSubmitting = true;
-    // TODO: remove nested subscribes, use mergeMap
+
 
     this.userService.isAuthenticated.pipe(concatMap(
       (authenticated) => {
@@ -32,7 +32,7 @@ export class FollowButtonComponent {
           return of(null);
         }
 
-        // Follow this profile if we aren't already
+
         if (!this.profile.following) {
           return this.profilesService.follow(this.profile.username)
           .pipe(tap(
@@ -43,7 +43,7 @@ export class FollowButtonComponent {
             err => this.isSubmitting = false
           ));
 
-        // Otherwise, unfollow this profile
+
         } else {
           return this.profilesService.unfollow(this.profile.username)
           .pipe(tap(
